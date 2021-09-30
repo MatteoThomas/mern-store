0e68e64a262499d814be09a943de0b3846946314
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-09-30 16:12:54 -0600

    initial

---
+++ src/data.js
@@ -0,0 +1,27 @@
+import kc from "../src/images/kc_doll.jpg";
+import mirror from "../src/images/mirror.jpg";
+import house from "../src/images/house.jpg";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: kc,
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON THIS SALE! GET 30% OFF NEW ITEMS!",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: mirror,
+    title: "WINTER SALE",
+    desc: "CHECK THESE COLD WEATHER FAVORITES!",
+    bg: "f3cacd",
+  },
+  {
+    id: 3,
+    img: house,
+    title: "POPULAR",
+    desc: "CHECK OUT OUR BEST SELLERS NOW!",
+    bg: "A3cabd",
+  },
+];
