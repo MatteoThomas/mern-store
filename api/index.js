212129e904cc93402774e942a6fd1cadd7af183f
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-10-10 17:19:17 -0600

    heroku server fix2

--- api/index.js
+++ api/index.js
@@ -11,11 +11,11 @@ const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "client", "public", "index.html"));
   });
 }
 
