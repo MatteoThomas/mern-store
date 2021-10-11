9a145f6b9e0d7b4bbe901ebfd4d8bb867898e35d
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-10-10 18:13:13 -0600

    heroku server fix8 changed cors origin, build / dir

--- api/index.js
+++ api/index.js
@@ -9,11 +9,14 @@ const productRoute = require("./routes/product");
 const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
+const path = require("path");
 
 dotenv.config();
 
+const MONGODB_URI = process.env.MONGODB_URI;
+
 mongoose
-  .connect(process.env.MONGODB_URI)
+  .connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("ecommerce DB Connection Access"))
   .catch((err) => {
     console.log(err);
@@ -22,6 +25,8 @@ mongoose
 app.use(
   cors({
     // origin: "http://localhost:5000",
+    origin: "https://ecomm-store-mern.herokuapp.com/",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
   })
 );
 app.use(express.json());
@@ -35,9 +40,8 @@ app.use("/api/checkout", stripeRoute);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 
-  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "/build/index.html"));
   });
 }
 
