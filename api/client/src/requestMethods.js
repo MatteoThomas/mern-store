062614aad3fa7007d270869f6fba556e726e1548
Matthew Elliott <77035998+MatteoThomas@users.noreply.github.com>
2021-10-10 20:07:11 -0600

    rm config.js

--- api/client/src/requestMethods.js
+++ api/client/src/requestMethods.js
@@ -1,14 +1,14 @@
-import { axiosInstance } from "./config";
+import axios from "axios";
 
-const BASE_URL = "https://ecomm-store-mern.herokuapp.com/";
+const BASE_URL = "https://ecomm-store-mern.herokuapp.com/api/";
 const TOKEN =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYxNWI2MjEzZDE4ZmRhZTliM2UxMGJlZCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTYzMzQ1ODMyNiwiZXhwIjoxNjMzNTQ0NzI2fQ.d1zq8_tpi1vTVEcGcYl8q0_Md6Jj7rIXucd05y2WDZU";
 
-export const publicRequest = axiosInstance.create({
+export const publicRequest = axios.create({
   baseURL: BASE_URL,
 });
 
-export const userRequest = axiosInstance.create({
+export const userRequest = axios.create({
   baseURL: BASE_URL,
   header: { token: `Bearer ${TOKEN}` },
 });
